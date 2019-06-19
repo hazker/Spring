831b77472d7cb014833b22feb981b676a4432431
hazker <zov.zon@gmail.com>
2019-06-20 01:03:25 +0700

    Update GetUser.java

--- src/main/java/ru/hello/GetUser.java
+++ src/main/java/ru/hello/GetUser.java
@@ -14,11 +14,6 @@ class GetUser{
 
     private final long id;
     private final String content;
-    /*private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost/eltex";
-    private static final String username = "root";
-    private static final String password = "bkmzxtujitd";
-    private static final String MAX_POOL = "250";*/
 
 
     public GetUser(long id, String content) {
